a701af3208145b1e9e6c8a193c095b255a081a37
Russel <russel.jma@gmail.com>
2021-01-28 23:22:24 -0800

    Added loss graphing

---
+++ graph.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import sys
+
+path = "trial"+sys.argv[1]+"/during_training_performance.txt"
+
+f = open(path)
+read = f.read()
+splits = read.split("\n")
+splits.remove("")
+
+index = []
+value = []
+for s in splits:
+    split = s.split(" ")
+    index.append(int(split[0]))
+    value.append(float(split[int(sys.argv[2])]))
+
+plt.plot(index, value)
+plt.show()

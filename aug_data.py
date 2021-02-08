0b09e70828cf8fc0524a6828b9a9ec5cf44d03d0
Russel <russel.jma@gmail.com>
2021-02-07 23:10:29 -0800

    Fixed re-augmentation in aug_data.py

--- aug_data.py
+++ aug_data.py
@@ -10,12 +10,12 @@ import os
 
 NUM_AUGS = 3
 
-train_data = torch.load("TRAIN_aquarium.pt") #(15100, 3, 256, 256)
-train_data = torch.clamp(train_data[torch.randperm(train_data.size(0))], 0, 1)
-valid_data = train_data[15000:15100]
-train_data = train_data[0:15000]
+train_data_load = torch.load("TRAIN_aquarium.pt") #(15100, 3, 256, 256)
+train_data_load = torch.clamp(train_data_load[torch.randperm(train_data_load.size(0))], 0, 1)
+valid_data = train_data_load[15000:15100]
+train_data = train_data_load[0:15000]
 for i in range(NUM_AUGS):
-    aug = torch.from_numpy(elasticdeform.deform_random_grid(train_data.numpy(), sigma=0.8, points=3, axis=(2, 3)))
+    aug = torch.from_numpy(elasticdeform.deform_random_grid(train_data_load[0:15000].numpy(), sigma=0.8, points=3, axis=(2, 3)))
     train_data = torch.cat((train_data, aug), dim=0)
     print("Augmentation done : "+str(i+1))
 print(train_data.size())

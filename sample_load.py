f5008df1407517a1dbf1919573e0e6a80e6e40c5
RArbore <russel.jma@gmail.com>
2021-04-25 13:47:55 -0700

    Optimized sample loading

--- sample_load.py
+++ sample_load.py
@@ -12,10 +12,14 @@ DATA_SIZE = 15100
 f = open("paths.txt", "r")
 paths = f.read().split("\n")[:-1]
 tensor_list = []
+length = len(paths)
 
 for i in tqdm.tqdm(range(DATA_SIZE)):
-    choice = random.choice(paths)
-    paths.remove(choice)
+    length -= 1
+    choice_i = random.randint(0, length)
+    choice = paths[choice_i]
+    paths.pop(choice_i)
+
     pil_img = Image.open("images256/"+choice)
     tensor = transforms.ToTensor()(pil_img)
     if (tensor.size(0) == 1):
@@ -25,4 +29,4 @@ for i in tqdm.tqdm(range(DATA_SIZE)):
 if tensor_list:
     data = torch.stack(tensor_list).float().detach()
     print(data.size())
-    torch.save(data, "TRAIN_sampled.pt")
+    torch.save(data, "TRAIN_DATA.pt")

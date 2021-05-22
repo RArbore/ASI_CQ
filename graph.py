68e864bfb5f96bf393461d3b683a390972b06840
RArbore <russel.jma@gmail.com>
2021-05-22 11:28:43 -0700

    Modified graph.py to be more flexible

--- graph.py
+++ graph.py
@@ -1,31 +1,31 @@
 import matplotlib.pyplot as plt
 import sys
 
-NUM_TRIALS = 2
-TRIAL_OFFSET = 15
-
 cm = plt.get_cmap('gist_rainbow')
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.set_prop_cycle('color', [cm(1.*i/NUM_TRIALS) for i in range(NUM_TRIALS)])
 
-for t in range(NUM_TRIALS):
-    path = "trial"+str(t+1+TRIAL_OFFSET)+"/during_training_performance.txt"
+path =sys.argv[1]+"/during_training_performance.txt"
 
-    f = open(path)
-    read = f.read()
-    splits = read.split("\n")
-    splits.remove("")
+f = open(path)
+read = f.read()
+splits = read.split("\n")
+splits.remove("")
 
-    index = []
-    value = []
-    for s in splits:
-        split = s.split(" ")
-        index.append(int(split[0]))
-        value.append(float(split[int(sys.argv[1])]))
+index = []
+value = []
+for s in splits:
+    split = s.split(" ")
+    index.append(int(split[0]))
+    value.append(float(split[int(sys.argv[2])]))
 
-    ax.plot(index, value)#, label=str(t+1))
+ax.plot(index, value)#, label=str(t+1))
 #plt.legend(loc="upper left")
-plt.ylabel("Validation Loss (MSE)")
+if sys.argv[2] == "0":
+    plt.ylabel("Epoch #")
+elif sys.argv[2] == "1":
+    plt.ylabel("Training Loss (MSE)")
+elif sys.argv[2] == "2":
+    plt.ylabel("Validation Loss (MSE)")
 plt.xlabel("Epoch #")
-plt.show()
+plt.savefig("Figure_"+sys.argv[1].split("_")[0]+"_"+sys.argv[1][-1]+".png")
